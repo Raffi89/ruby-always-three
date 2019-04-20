3e96755d76cfc66aacaaef5b74a36c10284cfe87
raffi <raffi.matcha@gmail.com>
2019-04-20 14:19:39 +0200

    Always three script

---
+++ always_three.rb
@@ -0,0 +1,10 @@
+# Always 3 Ruby script
+puts "Give me a number"
+first_number = gets.to_i
+mid_number = first_number + 5
+mid_number = mid_number * 2
+mid_number = mid_number - 4
+mid_number = mid_number / 2
+final_number = mid_number - first_number
+puts "The final number will Always be #{final_number}"
+#end of the script
